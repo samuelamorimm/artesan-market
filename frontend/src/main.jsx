56f3babbcb5c61a7364025f593f2c22bd97408bf
samuelamorimm <samuelhenriqueamorim@gmail.com>
2025-05-14 17:20:30 -0300

    update tela de inicio

---
+++ frontend/src/main.jsx
@@ -0,0 +1,7 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import Inicio from './pages/Inicio/index.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <Inicio></Inicio>
+)
---
+++ frontend/src/pages/Inicio/index.jsx
@@ -0,0 +1,43 @@
+import './index.css'
+import Inicial from '../../assets/inicial.png'
+
+export default function Inicio() {
+    return (
+        <>
+            <nav>
+                <div id='logo-area'>
+
+                </div>
+
+                <div id='link-area'>
+                    <a href="" className="link">Início</a>
+                    <a href="" className="link">Sobre</a>
+                    <a href="" className="link">Contato</a>
+                    <a href="" className="link">Duvidas</a>
+                </div>
+
+                <div id='btn-area'>
+                    <button className="btn-login">
+                        Entrar
+                    </button>
+                    <button className="btn-login">
+                        Cadastrar
+                    </button>
+                </div>
+            </nav>
+
+            <main>
+                <section id='section-img'>
+                    <img src={Inicial} alt="Homem fabricando jarro" />
+                </section>
+                <section id='section-txt'>
+                    <h1 id='titulo'>Artisan Market</h1>
+                    <p id='sobre'>Oferecemos ao usuário a melhor experiência de compra e venda. E um marketplace totalmente focado no artesanato, tanto para você vendedor quanto ao consumidor. Experimente!
+                    </p>
+
+                    <button id='btn-start'>Começar</button>
+                </section>
+            </main>
+        </>
+    )
+}
